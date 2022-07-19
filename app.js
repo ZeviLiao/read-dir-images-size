63c5a2a0a014bb073b241a456d83a8a1b2018d3d
Zevi Liao <jointjm@gmail.com>
2022-07-19 22:40:48 +0800

    image dimensions output to json

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const { join, resolve } = require('path');
 const sizeOf = require('image-size')
 var fs = require('fs');
 
-var dir = '/Users/zeviliao/KMprojects/dgpals-portal/public/asset/main/partners/partner-logo'
+var dir = 'your/images/filepath'
 
 const ls = fs.readdirSync(dir, { withFileTypes: true })
     .filter(item => !item.isDirectory())
     .map(item => item.name)
 
-for (img of ls) {
-    const path = join(dir, img);
-    console.log(path)
-    sizeOf(path, function (err, dimensions) {
-        console.log(img, dimensions.width, dimensions.height)
+// for (img of ls) {
+//     const path = join(dir, img);
+//     const dimensions = sizeOf(path)
+//     console.log(img, dimensions.width, dimensions.height)
+// }
+
+    const jsObj = ls.map(img => {
+        const path = join(dir, img);
+        const dimensions = sizeOf(path)
+        return {
+            path: img,
+            width: dimensions.width,
+            height: dimensions.height
+        }
     })
-}
+    console.log(JSON.stringify(jsObj,null,'\t'))
 
 
 
